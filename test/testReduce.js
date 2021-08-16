3168b661b46d357e271d246c335ce782a92b053a
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-08-16 09:57:21 +0530

    Add startingValue features in callback and initilize it by 5.

--- test/testReduce.js
+++ test/testReduce.js
@@ -3,14 +3,10 @@ const items = require('../data/items');
 const reduceFunction = require('../reduce');
 
 
-const result =  reduceFunction(items, (element)=>{
-     
-         if(items.includes(element)){
-             return true;
-         }    
-  
-  return false;       
-});
+const result =  reduceFunction(items, (element, startingValue)=>{
+                            
+        return startingValue += element       
+}, 5);
 
 
 console.log(result);
